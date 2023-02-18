14b5443f97768b479b5b6c655952c6077abc5778
tanyaka <litium@yandex.ru>
2023-02-18 13:28:30 +0300

    добавлен JS обработки кнопок

--- pages/script.js
+++ pages/script.js
@@ -1,16 +1,43 @@
 
-let EditButton = document.querySelector('.profile__edit');
-let CloseEdit = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
+let EditButton = document.querySelector('.profile__edit'); //выбираем кнопку редактирование
+let CloseEdit = document.querySelector('.popup__close'); //выбираем кнопку закрыть popup
 
+let popup = document.querySelector('.popup'); //выбор самого popup для добавления и удаления класса popup_opened
+
+
+// переменные Имени и О себе
+let userName = document.querySelector('.profile__name');
+let userAbout = document.querySelector('.profile__subtitle');
+
+let formElement = document.querySelector('.popup__form'); // переменная формы ввода
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector("[id='name']"); // выбираем елемент input по id name
+let aboutInput = formElement.querySelector("[id='about']");  // выбираем елемент input по id about
+nameInput.value = userName.textContent;
+aboutInput.value = userAbout.textContent;
+
+//обработка открытия и закрытия popup
 function togglePopup() {
-  popup.classList.toggle('popup_opened');
+  popup.classList.toggle('popup_opened'); //класс содержит свойство видимости
 }
 
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+     userName.textContent = nameInput.value;
+    userAbout.textContent = aboutInput.value ;
+    togglePopup();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
 EditButton.addEventListener('click', togglePopup);
 CloseEdit.addEventListener('click', togglePopup);
 
 
+//здесь долждна быть обработка нажатия на Лайк
 // let LikeButton = document.querySelectorAll('.element_like');
 // function addLike(){
 // LikeButton.classList.toggle('like_active');
@@ -18,27 +45,3 @@ CloseEdit.addEventListener('click', togglePopup);
 
 // LikeButton.addEventListener('click', addLike);
 
-
-// // Находим форму в DOM
-// let formElement = // Воспользуйтесь методом querySelector()
-// // Находим поля формы в DOM
-// let nameInput = // Воспользуйтесь инструментом .querySelector()
-// let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-// // Обработчик «отправки» формы, хотя пока
-// // она никуда отправляться не будет
-// function handleFormSubmit (evt) {
-//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-//                                                 // Так мы можем определить свою логику отправки.
-//                                                 // О том, как это делать, расскажем позже.
-
-//     // Получите значение полей jobInput и nameInput из свойства value
-
-//     // Выберите элементы, куда должны быть вставлены значения полей
-
-//     // Вставьте новые значения с помощью textContent
-// }
-
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
-// formElement.addEventListener('submit', handleFormSubmit);
