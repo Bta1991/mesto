efd7297ce6050f12195f959e04135689e8bc0ed6
tanyaka <litium@yandex.ru>
2023-03-04 20:47:12 +0300

    добавлены обработчики основных нажатий, кнопка удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,148 @@
-let editButton = document.querySelector('.profile__edit'); //выбираем кнопку редактирование
-let closeEdit = document.querySelector('.popup__close'); //выбираем кнопку закрыть popup
+//шаблон для начальных картинок
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    },
+]
 
-let elementPopup = document.querySelector('.popup'); //выбор самого popup для добавления и удаления класса popup_opened
+//выбор popup
+const popupEdit = document.querySelector('.popup_edit')
+const popupAdd = document.querySelector('.popup_add')
+const popupView = document.querySelector('.popup_view')
+
+//выберем кнопки закрыть
+const closeEdit = popupEdit.querySelector('.popup__close')
+const closeAdd = popupAdd.querySelector('.popup__close')
+const closeView = popupView.querySelector('.popup__close')
+
+//выбор форм
+const formAdd = popupAdd.querySelector('.popup__form')
+const formEdit = popupEdit.querySelector('.popup__form')
+
+const editButton = document.querySelector('.profile__edit') //выбираем кнопку редактирование
+const addButton = document.querySelector('.profile__add-button') //выбираем кнопку добавить картинку
+//нажали на просмотр фото
+
+// Находим поля формы релдактирования информации о себе в DOM
+let nameInput = formEdit.querySelector('.popup__input_data_name') // выбираем елемент имя
+let aboutInput = formEdit.querySelector('.popup__input_data_about') // выбираем елемент о себе
 
 // переменные Имени и О себе
-let userName = document.querySelector('.profile__name');
-let userAbout = document.querySelector('.profile__subtitle');
-// форма ввода
-let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__input_data_name'); // выбираем елемент имя
-let aboutInput = formElement.querySelector('.popup__input_data_about');  // выбираем елемент о себе
+let userName = document.querySelector('.profile__name')
+let userAbout = document.querySelector('.profile__subtitle')
 
+// Находим поля формы добавления фото
+let urlInput = formAdd.querySelector('.popup__input_data_url') // выбираем елемент имя
+let titleInput = formAdd.querySelector('.popup__input_data_title') // выбираем елемент о себе
 
-//открываем попап и вносим данные в форму с экрана
-function openPopup() {
-  nameInput.value = userName.textContent;
-  aboutInput.value = userAbout.textContent;
-  elementPopup.classList.add('popup_opened'); //класс содержит свойство видимости
-}
-//закрытие popup
-function closePopup() {
-  elementPopup.classList.remove('popup_opened'); //класс содержит свойство видимости
+//функция изменения видимости
+function togglePopup(target) {
+    target.classList.toggle('popup_opened') //класс содержит свойство видимости
 }
 
-// Обработчик «отправки» формы
-function handleFormSubmit (evt) {
-  evt.preventDefault();
-  userName.textContent = nameInput.value;
-  userAbout.textContent = aboutInput.value ;
-  closePopup();
+const userTemplate = document.querySelector('#element').content
+const userElements = document.querySelector('.elements')
+
+function addPhoto(inputsrc, inputtext) {
+    // клонируем содержимое тега template
+    const userElement = userTemplate.querySelector('.element').cloneNode(true)
+
+    // наполняем содержимым
+    userElement.querySelector('.element__photo').src = inputsrc
+    userElement.querySelector('.element__text').textContent = inputtext
+
+    likeListener(userElement.querySelector('.element__like')) // listener для лайка
+    trashListener(userElement.querySelector('.element__trash')) // listener для удаления
+    photoListener(userElement.querySelector('.element__photo')) // listener для фоток
+    // отображаем на странице
+    userElements.insertBefore(userElement, userElements.firstChild)
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
-//открытие окна редактирования
-editButton.addEventListener('click', openPopup);
-//закрытие окна
-closeEdit.addEventListener('click', closePopup);
+// обработчики и инициализация страницы----------------------------------------------------------------
+initialCards.forEach((card) => {
+    addPhoto(card.link, card.name)
+})
 
+// универсальная функция добавления listener для кнопки лайка
+function likeListener(like) {
+    like.addEventListener('click', function (evt) {
+        // в переменной eventTarget окажется элемент
+        const eventTarget = evt.target
+        eventTarget.classList.toggle('element__like_active')
+    })
+}
+//  функция добавления listener для кнопки корзины
+function trashListener(trash) {
+    trash.addEventListener('click', function (evt) {
+        // в переменной eventTarget окажется элемент
+        const eventTarget = evt.target
+        const parent = eventTarget.parentNode.parentNode
+        parent.remove()
+    })
+}
+//  функция добавления listener для фото
+function photoListener(photo) {
+    photo.addEventListener('click', function (evt) {
+        // в переменной eventTarget окажется элемент
+        const eventTarget = evt.target
+        togglePopup(popupView)
+    })
+}
 
-//здесь долждна быть обработка нажатия на Лайк
-// let LikeButton = document.querySelectorAll('.element_like');
-// function addLike(){
-// LikeButton.classList.toggle('like_active');
-// }
+//открытие и закрытие окна редактирования информации
+editButton.addEventListener('click', function () {
+    nameInput.value = userName.textContent //при открытии записываем в значение то что на экране
+    aboutInput.value = userAbout.textContent
+    togglePopup(popupEdit)
+})
+closeEdit.addEventListener('click', function () {
+    togglePopup(popupEdit)
+})
+// обработчик формы для окна редактирования информации
+formEdit.addEventListener('submit', function handleFormSubmit(evt) {
+    evt.preventDefault()
+    userName.textContent = nameInput.value
+    userAbout.textContent = aboutInput.value
+    togglePopup(popupEdit)
+})
 
-// LikeButton.addEventListener('click', addLike);
+//открытие закрытие окна добавления картинки
+addButton.addEventListener('click', function () {
+    togglePopup(popupAdd)
+})
+closeAdd.addEventListener('click', function () {
+    togglePopup(popupAdd)
+})
+// обработчик формы для загрузки фото
+formAdd.addEventListener('submit', function handleFormSubmit(evt) {
+    evt.preventDefault()
+    addPhoto(urlInput.value, titleInput.value)
+    evt.target.reset()
+    togglePopup(popupAdd)
+})
+
+//открытие закрытие превью фото
+closeView.addEventListener('click', function () {
+    togglePopup(popupView)
+})
